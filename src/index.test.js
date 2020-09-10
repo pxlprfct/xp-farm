bd4be94ed1e60afeff3a049130399b0e0977aa76
Ollie Williams <ollie.williams@ibm.com>
2020-09-10 17:02:44 +0100

    chore: updated test to use fixture

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,11 @@
-const mockConsole = jest.spyOn(global.console, 'log');
-
 const { startGame } = require('./index');
+const { START_GAME } = require('./test/strings');
 
 describe(`Tic-tac-toe is a game for two players, 'X' and 'O', who take turns marking the spaces in an empty 3×3 grid`, () => {
+  const mockConsole = jest.spyOn(global.console, 'log');
+
   test(`A new game starts on an empty 3x3 grid`, () => {
     startGame();
-    expect(mockConsole).toHaveBeenCalled();
+    expect(mockConsole).toHaveBeenCalledWith(START_GAME);
   });
 });
