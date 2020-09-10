b0a29b71e2ae8a08c560efd24fba9914e811c500
Ollie Williams <ollie.williams@ibm.com>
2020-09-10 17:03:50 +0100

    feat: added render board method

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
-const NEW_GAME = `
-Game Board Creation...
-| |
+const renderBoard = (board) => `
+${board[0][0]}|${board[0][1]}|${board[0][2]}
 -+-+-
-| |
+${board[1][0]}|${board[1][1]}|${board[1][2]}
 -+-+-
-| |
+${board[2][0]}|${board[2][1]}|${board[2][2]}`;
 
-Board Created.
-The game will start with player X
-`;
+const GAME_BOARD_CREATION = `Game Board Creation...`;
+const BOARD_CREATED = `Board Created.`;
+const START_WITH_PLAYER = `The game will start with player X`;
+
+const START_GAME_MESSAGE = (BOARD) => {
+  console.log(`
+${GAME_BOARD_CREATION}
+${renderBoard(BOARD)}
+\n
+${BOARD_CREATED}
+${START_WITH_PLAYER}`);
+};
 
 const startGame = async () => {
-  console.log(NEW_GAME);
+  const BOARD = Array.from(Array(3), () => new Array(3).fill(' '));
+
+  START_GAME_MESSAGE(BOARD);
+  BOARD.push(BOARD);
+
+  return BOARD;
 };
+
+startGame();
 
 module.exports = {
   startGame,
