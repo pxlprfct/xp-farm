317093cc4452d0e7275c5ea9b9a7129f6dc6eeb9
Ollie Williams <ollie.williams@ibm.com>
2020-09-14 19:50:22 +0100

    chore: moved tic-tac-toe into dedicated folder structure

--- white-belt/src/index.js
+++ white-belt/src/index.js
@@ -5,8 +5,6 @@ const delay = require('./helpers/delay');
 const autoStart = async () => {
   let currentState = startGame();
 
-  await delay(2000);
-
   while (currentState) {
     await delay(2000);
     currentState = nextTurn(currentState);
--- white-belt/src/tic-tac-toe.js
+++ white-belt/src/tic-tac-toe.js
@@ -1,17 +1,16 @@
 const { X, O, EMPTY } = require('./helpers/constants');
-
 const makeMove = require('./helpers/makeMove');
+const isWinner = require('./helpers/isWinner');
+const ableToMakeMove = require('./helpers/ableToMakeMove');
 const START_GAME_MESSAGE = require('./helpers/output/start_game');
 const WINNING_GAME_MESSAGE = require('./helpers/output/winning_game');
 const TURN_MESSAGE = require('./helpers/output/turn');
 const DRAW_MESSAGE = require('./helpers/output/draw');
-const isWinner = require('./helpers/isWinner');
-const ableToMakeMove = require('./helpers/ableToMakeMove');
 
 const togglePlayer = (player) => (player === X ? O : X);
 
 const startGame = () => {
-  const emptyBoard = Array.from(Array(3), () => new Array(3).fill(' '));
+  const emptyBoard = Array.from(Array(3), () => new Array(3).fill(EMPTY));
 
   console.log(START_GAME_MESSAGE(emptyBoard));
 
@@ -22,31 +21,31 @@ const startGame = () => {
 };
 
 const nextTurn = (state) => {
-  const { currentBoard } = state;
+  const { currentBoard, currentPlayer } = state;
 
   const [row, column] = makeMove();
 
   if (currentBoard[row][column] === EMPTY) {
-    currentBoard[row][column] = state.currentPlayer;
+    currentBoard[row][column] = currentPlayer;
   } else {
     nextTurn(state);
   }
 
-  if (isWinner(currentBoard, state.currentPlayer)) {
-    console.log(WINNING_GAME_MESSAGE(currentBoard, state.currentPlayer));
+  if (isWinner(currentBoard, currentPlayer)) {
+    console.log(WINNING_GAME_MESSAGE(currentBoard, currentPlayer));
     return false;
   }
 
   if (!ableToMakeMove(currentBoard)) {
-    console.log(DRAW_MESSAGE(currentBoard, state.currentPlayer));
+    console.log(DRAW_MESSAGE(currentBoard, currentPlayer));
     return false;
   }
 
-  console.log(TURN_MESSAGE(currentBoard, state.currentPlayer));
+  console.log(TURN_MESSAGE(currentBoard, currentPlayer));
 
   return {
     currentBoard,
-    currentPlayer: togglePlayer(state.currentPlayer),
+    currentPlayer: togglePlayer(currentPlayer),
   };
 };
 
